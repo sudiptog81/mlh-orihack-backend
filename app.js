b84401c6b37cee92924800d14214f9372cf33f82
Desire Kaleba <desirekaleba@gmail.com>
2021-06-09 08:19:31 +0300

    feat: Add passport to app

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+require("./passport");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+app.use(
+  cookieSession({
+    name: "github-auth-session-app-name",
+    keys: ["key1", "key2"],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger("dev"));
 app.use(express.json());
