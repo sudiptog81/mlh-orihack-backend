b1e60df082027774c9caf0e35f484cb059843800
Desire Kaleba <desirekaleba@gmail.com>
2021-06-09 08:20:57 +0300

    feat: Add github authentication routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require("passport");
 
 const router = express.Router();
 
@@ -7,6 +8,43 @@ router.get("/", (req, res) => {
   res.send({
     message: "/",
   });
+});
+/* GET dashboard page. */
+router.get("/dashboard", (req, res) => {
+  res.send({
+    message: "/dashboard",
+  });
+});
+
+/**
+ * On authentication error route.
+ */
+router.get("/auth/error", (req, res) => res.send("Unknown Error"));
+/**
+ * Github authentication route.
+ */
+router.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] })
+);
+/**
+ * On authentication success route.
+ */
+router.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/auth/error" }),
+  (req, res) => {
+    res.redirect("/dashboard");
+  }
+);
+
+/**
+ * Logout user.
+ */
+router.get("/logout", (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
