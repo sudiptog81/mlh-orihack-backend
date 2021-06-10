08bbd07fa47ce43c2112eb8a8e7806da9680e82c
Soham Parekh <soham.parekh1998@gmail.com>
2021-06-10 15:26:41 +0530

    chore: setup mongoose and winston

--- app.js
+++ app.js
@@ -2,28 +2,56 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo");
+const passport = require("passport");
 
-const { SESSION_SECRET } = require("./util/secrets");
+const initPassport = require("./util/passport");
+const logger = require("./util/logger");
+const { MONGODB_URL, SESSION_SECRET } = require("./util/secrets");
 
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
 
 const app = express();
 
+initPassport();
+
+mongoose.connect(
+  MONGODB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  },
+  (err) => {
+    if (err) {
+      logger.error("Error connecting to MongoDB", {
+        err,
+      });
+      process.exit(1);
+    }
+  }
+);
+
 const sessionOptions = {
   secret: SESSION_SECRET,
   resave: true,
   expires: new Date() * 60 * 60 * 24 * 7,
   saveUninitialized: true,
-  store: "",
+  store: MongoStore.create({
+    mongoUrl: MONGODB_URL,
+  }),
 };
 
 app.use(session(sessionOptions));
-app.use(logger("dev"));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
--- util/secrets.js
+++ util/secrets.js
@@ -1,15 +1,34 @@
-require("dotenv").config();
+require("dotenv-defaults").config();
+const logger = require("./logger");
 
 const {
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
   GITHUB_CALLBACK_URL,
+  MONGODB_URL,
+  NODE_ENV,
   SESSION_SECRET,
 } = process.env;
+
+const requiredCredentials = [
+  "GITHUB_CLIENT_ID",
+  "GITHUB_CLIENT_SECRET",
+  "GITHUB_CALLBACK_URL",
+  "MONGODB_URL",
+];
+
+for (const credential of requiredCredentials) {
+  if (!process.env[credential]) {
+    logger.error(`Missing required credentials ${credential}`);
+    process.exit(1);
+  }
+}
 
 module.exports = {
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
   GITHUB_CALLBACK_URL,
+  MONGODB_URL,
+  NODE_ENV,
   SESSION_SECRET,
 };
