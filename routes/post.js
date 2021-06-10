0f5a35c7e1f8d8ce068e19bd5531003d80f7ff55
Desire Kaleba <desirekaleba@gmail.com>
2021-06-11 01:53:10 +0300

    feat: add route to delete specif post

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getAll, add, findById } = require("../controllers/post");
+const { getAll, add, findById, deletePost } = require("../controllers/post");
 
 const router = express.Router();
 
 router.get("/", getAll);
 router.post("/", add);
 router.get("/:id", findById);
-
+router.delete("/:postId", deletePost);
 module.exports = router;
